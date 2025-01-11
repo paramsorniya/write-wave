6ae6405bee0d973240676adab9f27f2d0e4a9582
param <paramnama15@gmail.com>
2025-01-11 20:36:31 +0530

    cors issue taken

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,4 @@
 import dotenv from "dotenv";
-
 import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
@@ -10,35 +9,127 @@ import dbConnection from "./dbConfig/index.js";
 import router from "./routes/index.js";
 import errorMiddleware from "./middleware/errorMiddleware.js";
 import path from "path";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8800;
 
+// Database connection
 dbConnection();
 
+// Middleware
 app.use(helmet());
-app.use(cors());
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(morgan("dev"));
+// Configure CORS
+const allowedOrigins = ["https://writewave-admin.onrender.com"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true, // Allow cookies if needed
+  })
+);
 
+// Routes and Middleware
 app.use(router);
 app.use(errorMiddleware);
 
+// Serve frontend static files
 const __dirname = path.resolve();
-app.listen(PORT, () => {
-  console.log("Server running of port " + PORT);
-});
-
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
+
+// Start the server
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
+
+
+
+
+
+
+
+
+// import dotenv from "dotenv";
+
+// import bodyParser from "body-parser";
+// import cors from "cors";
+// import express from "express";
+// import mongoose from "mongoose";
+// import helmet from "helmet";
+// import morgan from "morgan";
+// import dbConnection from "./dbConfig/index.js";
+// import router from "./routes/index.js";
+// import errorMiddleware from "./middleware/errorMiddleware.js";
+// import path from "path";
+// dotenv.config();
+
+// const app = express();
+// const PORT = process.env.PORT || 8800;
+
+// dbConnection();
+
+// app.use(helmet());
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json({ limit: "10mb" }));
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(morgan("dev"));
+
+// app.use(router);
+// app.use(errorMiddleware);
+
+// const __dirname = path.resolve();
+// app.listen(PORT, () => {
+//   console.log("Server running of port " + PORT);
+// });
+
+// app.use(express.static(path.join(__dirname, "/client/dist")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // import dotenv from "dotenv";
 // import bodyParser from "body-parser";
