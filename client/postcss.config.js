2e866e55c125824c6f4551b05530db9f6376da7b
param <paramnama15@gmail.com>
2025-01-20 00:15:44 +0530

    install some dependencies

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     plugins: {
-      'postcss-import': {},
-      'tailwindcss/nesting': 'postcss-nesting',
+      'tailwindcss/nesting': {}, // Use Tailwind's built-in nesting support
       tailwindcss: {},
       autoprefixer: {},
-    }
-  }
+    },
+  };
+  
